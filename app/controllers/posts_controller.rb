e5c3da60127cff4838e19fa9e1e6c23119deaa4c
Getro Naissance and Margie Nevarez <getro@>
2017-02-15 15:35:38 -0800

    add update, delete functionality to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -18,6 +22,21 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
 private
