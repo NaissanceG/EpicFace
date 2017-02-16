531b3d83b51f086738d716aab94e3685b5557673
Getro Naissance and Margie Nevarez <margie@>
2017-02-15 16:05:08 -0800

    add partials, before_action to postsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def new
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def create
@@ -25,7 +26,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
+
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
@@ -34,7 +35,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    
     @post.destroy
     redirect_to posts_path
   end
@@ -43,5 +44,7 @@ private
   def post_params
     params.require(:post).permit(:caption, :image)
   end
-
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
