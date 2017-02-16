df58d6711209e2f7bbd1a2c6be722b1332f0f5ac
Getro Naissance and Margie Nevarez <getro@>
2017-02-16 13:54:03 -0800

    add own post method to prevent users from editing, deleting posts of others

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
+  
   def index
     @posts = Post.all
   end
@@ -50,7 +51,16 @@ private
   def post_params
     params.require(:post).permit(:caption, :image)
   end
+
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "Oops, you got the wrong post.. you can only access your own posts. Try again."
+      redirect_to root_path
+    end
+  end
+
 end
