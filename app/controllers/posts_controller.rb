443619f0c59c84040ec28099956525b6ac007618
Getro Naissance and Margie Nevarez <margie@>
2017-02-16 21:29:21 -0800

    add implement admin functionality to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :admin_post, only: [:edit, :update, :destroy]
   before_action :owned_post, only: [:edit, :update, :destroy]
-  
+
   def index
     @posts = Post.all
   end
@@ -56,11 +57,19 @@ private
     @post = Post.find(params[:id])
   end
 
+  def admin_post
+    unless current_user.admin == true
+      flash[:alert] = "You are not an admin."
+      redirect_to root_path
+    end
+  end
+
   def owned_post
     unless current_user == @post.user
       flash[:alert] = "Oops, you got the wrong post.. you can only access your own posts. Try again."
       redirect_to root_path
     end
   end
+
 
 end
